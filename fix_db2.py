a4fcb85330398f2990fc2a3a201a8370ee9306ab
Anndh55 <2509295490@qq.com>
2026-06-28 16:04:00 +0800

    chore: remove temp file

--- fix_db2.py
+++
@@ -1,8 +0,0 @@
-﻿with open("D:/project/an_huan/an-huan-site/src/lib/db.ts", "r", encoding="utf-8") as f:
-    c = f.read()
-old = '/\\b([a-z]+[A-Z][a-zA-Z0-9]*)\\b/g'
-new = '/(?<!")(' + '\\b[a-z]+[A-Z][a-zA-Z0-9]*\\b)/g'
-c = c.replace(old, new)
-with open("D:/project/an_huan/an-huan-site/src/lib/db.ts", "w", encoding="utf-8") as f:
-    f.write(c)
-print("Fixed")
